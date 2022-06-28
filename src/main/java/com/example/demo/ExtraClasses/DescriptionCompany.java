febae8b31049655d53f16d1ed41afa6d55b27542
Spini <murzyn200069@gmail.com>
2022-06-28 22:49:32 +0200

    Added companyProfile. Not completed yet.

---
+++ src/main/java/com/example/demo/ExtraClasses/DescriptionCompany.java
@@ -0,0 +1,17 @@
+package com.example.demo.ExtraClasses;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DescriptionCompany {
+
+    String companyName;
+    String email;
+    String website;
+    String companyLogo;
+    String description;
+
+}
---
+++ src/main/java/com/example/demo/repository/CompanyProfileRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.CompanyProfile;
+import com.example.demo.model.UserProfile;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface CompanyProfileRepository extends MongoRepository<CompanyProfile,Integer> {
+    Optional<CompanyProfile> findById(String id);
+    Optional<CompanyProfile> findByUserEmail(String email);
+}
